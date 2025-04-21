60c371efb0e1b55a499ca6bf640b291574445215
AntonioSantosBJPE <netoifpe@gmail.com>
2025-04-21 11:14:56 -0300

    feat: create ROUTES constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,12 @@
+export const ROUTES = {
+  HOME: '/',
+  SIGN_IN: '/sign-in',
+  SIGN_UP: '/sign-up',
+  FORGOT_PASSWORD: '/forgot-password',
+  RESET_PASSWORD: '/reset-password',
+  DASHBOARD: {
+    HOME: '/dashboard',
+    PROFILE: '/dashboard/profile',
+    SETTINGS: '/dashboard/settings',
+  },
+}
