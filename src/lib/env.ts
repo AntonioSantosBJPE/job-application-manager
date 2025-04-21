f27cacc278021b54de064dc86c0f48ed57e1ad0f
AntonioSantosBJPE <netoifpe@gmail.com>
2025-04-21 11:05:11 -0300

    feat: add initial better auth and prisma congfig

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 export const envSchema = z.object({
   server: z.object({
     DATABASE_URL: z.string(),
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.string(),
   }),
 
   client: z.object({
@@ -18,6 +20,8 @@ export const env = envSchema.parse({
   NODE_ENV: process.env.NODE_ENV,
   server: {
     DATABASE_URL: process.env.DATABASE_URL,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
   },
   client: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
